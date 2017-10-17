79d6338fb50157121776665966b0bdfd7ee950e1
omelyanchikd <omelyanchikd@ukr.net>
2017-10-17 13:53:35 +0200

    Refactoring run.py for new format of run_ace file

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-import ace
 import json
 
-with open('model_config.json', 'r') as f:
-    model_config = json.load(f)
-
-with open('run_config.json', 'r') as f:
-    run_config = json.load(f)
-
-
 from ace import run_ace
-run_ace.run_ace(model_config, run_config)
+run_ace.run_ace_from_file("model_config.json", "run_config.json")
